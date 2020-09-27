ba48bf46a3d058083307d1ae0df89c3dc47e79d4
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2020-09-27 22:17:07 +0600

    5. Variables
    5. Variables in java

---
+++ 5. Variables/src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args){
+
+            int a;
+            float b;
+            double c;
+            long d;
+            short e;
+            
+
+    }
+}
