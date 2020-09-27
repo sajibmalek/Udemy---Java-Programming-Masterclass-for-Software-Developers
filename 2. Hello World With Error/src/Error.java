48e1e34e376024bc1439983d992e6c224cf476fc
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2020-09-27 22:06:35 +0600

    Semicolon missed

--- 2. Hello World With Error/src/Error.java
+++ 2. Hello World With Error/src/Error.java
@@ -1,6 +1,6 @@
 public class Error {
     public static void main(String [] args){
-        System.out.println("Error With, Hello World!");
+        System.out.println("Error With, Hello World!")
     }
 
 }
