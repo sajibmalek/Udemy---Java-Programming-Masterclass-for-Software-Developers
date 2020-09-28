e0bd4d63bc19cd3f0cc96f92d230c8ab4f0a26ca
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2020-09-29 02:39:36 +0600

    char and String

---
+++ 13. The char and boolean Primitive Data Types/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 
+        char mychar='D';
+        mychar='\u0044';
+        System.out.println("mychar = " + mychar);
+        System.out.println("mychar = " + mychar);
+
+        String unicode= "\u2764";
+
+        System.out.println("unicode = " + unicode);
+        System.out.println("unicode = " + unicode);
+        System.out.println("unicode = " + unicode);
+        System.out.println("unicode = " + unicode);
+        System.out.println("unicode = " + unicode);
+
+
+
+
+    }
+}
