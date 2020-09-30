dd9ba27bddb2739116290a446d92578921e12235
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2020-09-30 06:21:08 +0600

    Reacp String

---
+++ 3. First Steps/14. Primitive Types Recap and the String Data Type/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	        // byte
+            //short
+            //int
+            //float
+            //long
+            //double
+            //char
+            //boolean
+        System.out.println();
+        String myString="This is my String";
+        System.out.println("This is my String = " + myString);
+        myString=myString + "," + "and this more String";
+        System.out.println("myString = " + myString);
+        myString=myString +" " + "\u00A9"+" " + "2020";
+        System.out.println("myString = " + myString);
+
+        String lastString="10.10";
+                int myInt=10;
+                lastString=lastString+myInt;
+        System.out.println("lastString = " + lastString);
+        
+
+
+
+    }
+}
