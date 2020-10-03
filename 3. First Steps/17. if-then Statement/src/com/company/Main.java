5d5d3afe36381f027509474e21fc40a0f366465c
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2020-10-04 04:45:55 +0600

    if else statement done

---
+++ 3. First Steps/17. if-then Statement/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a=2, b=4;
+
+        if(a>b){
+            System.out.println("a is gather than b");
+        }else{
+            System.out.println("b is gather than a");
+        }
+
+        boolean isAlien=false;
+        if (isAlien == true){
+            System.out.println("It is not an Alien!");
+        }
+        else {
+            System.out.println("It is an Alien!");
+        }
+
+    }
+}
