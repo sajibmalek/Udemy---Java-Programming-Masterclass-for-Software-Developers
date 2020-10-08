e35c305a193263055707873d381de21755529440
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2020-10-08 12:33:52 +0600

    logical operator

---
+++ 3. First Steps/18. Logical and Operator/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int topScore=100;
+        int passMark=33;
+
+        if(topScore==80 && topScore !=passMark)
+        {
+            System.out.println("A+");
+        }
+       
+    }
+}
