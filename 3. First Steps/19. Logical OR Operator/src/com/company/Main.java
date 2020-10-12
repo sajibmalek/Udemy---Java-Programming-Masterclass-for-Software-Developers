0a2889533a34ca94f075301633c53a3584fcf13f
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2020-10-12 13:23:43 +0600

    OR Operator done

---
+++ 3. First Steps/19. Logical OR Operator/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int topscore=100;
+        int lowScore=33;
+        if (topscore==100 || lowScore!=topscore){
+            System.out.println("Result");
+        }
+    }
+}
