265561a903070227a22d20193aeff3fe46a4a92e
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2020-10-12 13:16:01 +0600

    logic

--- 3. First Steps/18. Logical and Operator/src/com/company/Main.java
+++ 3. First Steps/18. Logical and Operator/src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int topScore=100;
-        int passMark=33;
+        int failMark=32;
 
-        if(topScore==80 && topScore !=passMark)
+        if(topScore==100)
         {
             System.out.println("A+");
         }
